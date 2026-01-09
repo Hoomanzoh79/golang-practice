e56af68d3def9fc14ed28b8462182c94cc79fbe0
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2026-01-09 17:28:32 +0330

    ✨ Add my first error handling practices

---
+++ go-tour/error-handlings.go
@@ -0,0 +1,10 @@
+package main
+
+import ("errors")
+
+func Division(num, division_by int) (int, error) {
+	if division_by == 0 {
+		return 0, errors.New("Zero division error")
+	}
+	return num / division_by,nil
+}
--- go-tour/main.go
+++ go-tour/main.go
@@ -1,26 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	// pos, neg := Adder(), Adder()
-	f := Fibo()
-	for i:= 0 ;i<10;i++{
-		// fmt.Println(
-		// 	pos(i),
-		// 	neg(-2*i),
-		// )
-		fmt.Println(f())
+	result,err := Division(6,0)
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
 	}
+	fmt.Println(result)
 }
-//results are 
-// 0 0
-// 1 -2
-// 3 -6
-// 6 -12
-// 10 -20
-// 15 -30
-// 21 -42
-// 28 -56
-// 36 -72
-// 45 -90
